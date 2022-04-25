cc557792efd8410f1ac2d9c7807541f179783328
lmoraesdev <lbc_moraes@outlook.com>
2022-04-25 17:25:40 -0300

    Finalizado

--- Js/ByteBank/ContaCorrente.js
+++ Js/ByteBank/ContaCorrente.js
@@ -1,23 +1,57 @@
-export class ContaCorrente{
+import { Cliente } from "./Cliente.js";
+
+export class ContaCorrente {
+	static numeroDaContas = 0;
+
 	agencia;
-	cliente;
+
+	_cliente;
+
+	get cliente() {
+		return this._cliente;
+	}
+
+	set cliente(value) {
+		this._cliente = value;
+	}
 
 	_saldo = 0;
 
-	sacar(valor){
-		if(this._saldo >= valor){
+	set cliente(novoValor) {
+		if (novoValor instanceof Cliente) {
+			this._cliente = novoValor;
+		}
+	}
+
+	get clinte() {
+		return this._cliente;
+	}
+
+	get saldo() {
+		return this._saldo;
+	}
+
+	sacar(valor) {
+		if (this._saldo >= valor) {
 			this._saldo -= valor;
 			return valor;
 		}
 	}
-	depositar(valor){
-		if(valor > 0){
-		this._saldo += valor;
+
+	depositar(valor) {
+		if (valor > 0) {
+			this._saldo += valor;
 		}
 	}
 
-	transferir(valor, conta){
+	transferir(valor, conta) {
 		const valorSacado = this.sacar(valor);
 		conta.depositar(valorSacado);
+	}
+
+	constructor(agencia, cliente) {
+		this.agencia = agencia;
+		this.cliente = cliente;
+		ContaCorrente.numeroDaContas++;
 	}
 }
--- Js/ByteBank/index.js
+++ Js/ByteBank/index.js
@@ -1,25 +1,10 @@
-import {Cliente} from './Cliente.js';
-import {ContaCorrente} from './ContaCorrente.js';
+import { Cliente } from "./Cliente.js";
+import { ContaCorrente } from "./ContaCorrente.js";
 
-const cliente1 = new Cliente();
-const cliente2 = new Cliente();
+const cliente1 = new Cliente("Joao", 12345678910);
+const cliente2 = new Cliente("Leandro", 98765432120);
 
+const contaCorrenteJoao = new ContaCorrente(1002, cliente1);
+const contaCorrenteLeandro = new ContaCorrente(1002, cliente2);
 
-cliente1.nome = "Joao";
-cliente1.cpf = 12345678910;
-
-cliente2.nome = "Leandro";
-cliente2.cpf = 88845678910;
-
-const contaCorrenteJoao = new ContaCorrente();
-contaCorrenteJoao.agencia = 1001;
-contaCorrenteJoao.cliente = cliente1;
-
-const contaCorrenteLeandro = new ContaCorrente();
-contaCorrenteLeandro.agencia = 1001;
-contaCorrenteLeandro.cliente = cliente2;
-
-console.log(contaCorrenteJoao);
-console.log(contaCorrenteLeandro);
-console.log();
-
+console.log(contaCorrenteJoao, contaCorrenteLeandro);
