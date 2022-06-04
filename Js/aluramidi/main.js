94d1f2fcb2989c4a6da8c78a6662391adb8818da
lmoraesdev <lmoraesdev@DESKTOP-UH1IO6N.localdomain>
2022-06-04 19:01:33 -0300

    estudo dom js

--- Js/aluramidi/main.js
+++ Js/aluramidi/main.js
@@ -1,7 +1,15 @@
-function tocaSomPom(){
-	document.querySelector('#som_tecla_pom').play();
+const listaDeTeclas = document.querySelectorAll(".tecla");
+
+function tocaSom(idElementoAudio) {
+	document.querySelector(idElementoAudio).play();
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+	const tecla = listaDeTeclas[contador];
+	const instrumento = tecla.classList[1];
+	const idAudio = `#som_${instrumento}`;
 
-
+	tecla.onclick = function () {
+		tocaSom(idAudio);
+	};
+}
