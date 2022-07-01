a20dfdfbf22c65637615c958d9608eba2578ed4e
lmoraesdev <lbc_moraes@outlook.com>
2022-07-01 18:06:25 -0300

    estudo de DOM

---
+++ Js/robotron-2000/js/main.js
@@ -0,0 +1,15 @@
+const controle = document.querySelectorAll(".controle-ajuste");
+
+controle.forEach((elemento) => {
+	elemento.addEventListener("click", (evento) => {
+		manipulaDados(evento.target.textContent);
+	});
+});
+
+function manipulaDados(operacao) {
+	if (operacao == "-") {
+		braco.value = parseInt(braco.value) - 1;
+	} else {
+		braco.value = parseInt(braco.value) + 1;
+	}
+}
