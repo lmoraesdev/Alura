674ffbe2e0138e1c5b106515bb7534df4a3c6fa1
lmoraesdev <lbc_moraes@outlook.com>
2022-07-01 19:49:10 -0300

    finalizado

--- Js/robotron-2000/js/main.js
+++ Js/robotron-2000/js/main.js
@@ -1,5 +1,5 @@
-const controle = document.querySelectorAll(".controle-ajuste");
-const estatisticas = document.querySelectorAll("[data-estatistica]");
+const controle = document.querySelectorAll("[data-controle]");
+const estatistica = document.querySelectorAll("[data-estatistica]");
 
 const pecas = {
 	bracos: {
@@ -37,25 +37,25 @@ const pecas = {
 
 controle.forEach((elemento) => {
 	elemento.addEventListener("click", (evento) => {
-		manipulaDados(evento.target.dataSet.controle, evento.target.parentNode);
-		atualizaEstatisticas(evento.target.dataset.peca);
-	});
+		manipulaDados(evento.target.dataset.controle, evento.target.parentNode);
+		atualizaEstatistica(evento.target.dataset.peca)
+	})
 });
 
 function manipulaDados(operacao, controle) {
 	const peca = controle.querySelector("[data-contador]");
 
-	if (operacao == "-") {
-		peca.value = parseInt(peca.value) - 1;
+	if (operacao === "-") {
+		peca.value = parseInd(peca.value) - 1;
 	} else {
 		peca.value = parseInt(peca.value) + 1;
 	}
 }
 
-function atualizaEstatisticas(peca) {
-	estatisticas.forEach((elemento) => {
+function atualizaEstatistica(peca) {
+	estatistica.forEach((elemento) => {
 		elemento.textContent =
 			parseInt(elemento.textContent) +
-			pecas[peca][elemento.dataSet.estatisticas];
+			pecas[peca][elemento.dataset.estatistica];
 	});
 }
